42c1f887269ee4efab13ec956fce553c88d1b804
Matheson Steplock <ikifar2012@users.noreply.github.com>
2022-01-05 16:50:27 -0500

    setting vars

--- amcrest2mqtt-addon/run.sh
+++ amcrest2mqtt-addon/run.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bashio
 
-# bashio example
-TEXT=$(bashio::config "text")
-echo ${TEXT}
+#set environment variables
+export AMCREST_HOST=$(bashio::config 'amcrest_host')
+export AMCREST_PORT=$(bashio::config 'amcrest_port')
+export AMCREST_USERNAME=$(bashio::config 'amcrest_username')
+export AMCREST_PASSWORD=$(bashio::config 'amcrest_password')
+export STORAGE_POLL_INTERVAL=$(bashio::config 'storage_poll_interval')
+export DEVICE_NAME=$(bashio::config 'device_name')
+export MQTT_HOST=$(bashio::config 'mqtt_host')
+export MQTT_QOS=$(bashio::config 'mqtt_qos')
+export MQTT_PORT=$(bashio::config 'mqtt_port')
+export MQTT_USERNAME=$(bashio::config 'mqtt_username')
+export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+export MQTT_TLS_ENABLED=$(bashio::config 'mqtt_tls_enabled')
+export MQTT_TLS_CA_CERT=$(bashio::config 'mqtt_tls_ca_cert')
+export MQTT_TLS_CERT=$(bashio::config 'mqtt_tls_cert')
+export MQTT_TLS_KEY=$(bashio::config 'mqtt_tls_key')
+export HOME_ASSISTANT=$(bashio::config 'home_assistant')
+export HOME_ASSISTANT_PREFIX=$(bashio::config 'home_assistant_prefix')
+
